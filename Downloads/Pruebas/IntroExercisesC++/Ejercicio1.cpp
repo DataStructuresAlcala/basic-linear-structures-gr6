cd527cf3343baaa4d6f8e001bf4a2e0730606841
Aarón Vicente Santos <aaron.vicente@edu.uah.es>
2019-10-20 21:37:26 +0200

    Update the things

---
+++ Downloads/Pruebas/IntroExercisesC++/Ejercicio1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+// EJERCICIO 1
+int main(int argc, char **argv)
+
+{
+	int num;
+	char let;
+
+	cout << "Dame un número: ";
+	cin >> num; // Aquí estamos pidiendo un valor por teclado
+
+	cout << "Dame una letra: ";
+	cin >> let; // Aquí también
+
+	cout << "Tu numero es este: " << num << " y tu letra es: " << let << endl;
+	
+	//Endl para ponerlo en otra línea.
+
+
+}
